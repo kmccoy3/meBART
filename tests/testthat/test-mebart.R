a40fedb3a0fc53cfa155a93eb84bf42b3a6c8c4d
kmccoy3 <kevinmccoy678@gmail.com>
2025-04-10 15:30:41 -0500

    add testing back

--- tests/testthat/test-mebart.R
+++ tests/testthat/test-mebart.R
@@ -1,38 +1,66 @@
 
-########## Test 1 ##########
+########## Initial Test ##########
 
-x <- 2 + 3
+test_that("test_that framework is working", {
+    expect_identical(2+3, 5)
+})
 
-expect_equal(x, 5)
-
-########## Test 1 ##########
+########## meBART Test ##########
 
 f = file()
 sink(file=f)
 
+# Fix random seed to make the results reproducible
+set.seed(0)
 
+# number of observations
+n <- 50
 
-# Data
+# variance of measurement error and output, respectively
+sigma_meas <- 0.1
+sigma_y <- 0.1
 
-x = MASS::Boston[,c(6,13)] #rm=number of rooms and lstat= percent lower status
-y = MASS::Boston$medv # median value
+# Generate x values uniformly and add measurement error
+x_true <- runif(n, 0, 1)
+x_meas <- x_true + rnorm(n, 0, sigma_meas)
+y_true <- 1 * (x_true >= 0.5)
+y_obs <- y_true + rnorm(n, 0, sigma_y)
 
-set.seed(99)  ## MCMC posterior sampling: set seed for reproducibility
-nd=200        ## number of draws to keep
-burn=50      
+# Convert to df
+df_me <- data.frame(X=x_meas, y=y_obs)
+# df_no_me <- data.frame(X=x_true, y=y_obs)
 
-# Run two different BARTS
+# Parameters
+ndpost <- 500 # (Default: 1000L)
+nskip <- 100 # (Default: 100L)
+ntree <- 100 # (Default: 200L)
 
+proposal_sd = 0.10
 
- ## silence upcoming output using anonymous file connection
+# Data WITH Measurement Error
+set.seed(0)
+me_mdl <- meBART::mebart(df_me$X, df_me$y, 
+                         nskip=nskip, ndpost=ndpost, ntree=ntree, 
+                         proposal_sd=proposal_sd)
 
-bf1 = invisible(meBART::wbart(x, y, nskip=burn, ndpost=nd))
-bf1 <- bf1[-9]
+old_mdl <- readRDS("me_mdl.rds")
 
-set.seed(99)
-bf2 = invisible(meBART::mebart(x, y, nskip=burn, ndpost=nd))
-bf2 <- bf2[-9]
 sink()
 
-expect_equal(bf1, bf2)
+
+test_that("meBART objects are identical to previous commits", {
+    expect_equal(me_mdl$sigma, old_mdl$sigma)
+    expect_equal(me_mdl$yhat.train.mean, old_mdl$yhat.train.mean)
+    expect_equal(me_mdl$yhat.train, old_mdl$yhat.train)
+    expect_equal(me_mdl$varcount, old_mdl$varcount)
+    expect_equal(me_mdl$varprob, old_mdl$varprob)
+    expect_equal(me_mdl$treedraws, old_mdl$treedraws)
+    expect_equal(me_mdl$x_draws, old_mdl$x_draws)
+    expect_equal(me_mdl$acceptances, old_mdl$acceptances)
+    expect_equal(me_mdl$mu, old_mdl$mu)
+    expect_equal(me_mdl$varcount.mean, old_mdl$varcount.mean)
+    expect_equal(me_mdl$varprob.mean, old_mdl$varprob.mean)
+    expect_equal(me_mdl$rm.const, old_mdl$rm.const)
+})
+
 
