c9822152ab7b63f0be324f3e2fa0a9dba0b31b55
kmccoy3 <kevinmccoy678@gmail.com>
2025-04-10 15:19:46 -0500

    Add DynLib registration, Rcpp evalCpp back in, general Royxgen2 bugs

--- R/meBART-package.R
+++ R/meBART-package.R
@@ -1,6 +1,12 @@
-#' @keywords internal
+#' @keywords internal 
 "_PACKAGE"
-
-## usethis namespace: start
-## usethis namespace: end
+#' meBART: Bayesian Additive Regression Trees with Measurement Error
+#'
+#' Bayesian Additive Regression Trees (BART) sum-of-trees model expanded to incorporate 
+#' measurement errors in the independent variables.
+#' 
+#'
+#' @name meBART
+#' @importFrom Rcpp evalCpp
+#' @useDynLib meBART, .registration=TRUE
 NULL
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 
 # Bayesian Additive Regression Trees with Measurement Error (meBART)
 
+# Notes:
+
+To add manual:
+`devtools::build_manual()`
+
+To re-do Roxygen documentation:
+`devtools::document()`
+
+Add documentation for whole package:
+`use_package_doc()`
