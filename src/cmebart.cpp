3ce78614867b7ed9ccaf622a8dcef403bdb930fb
kmccoy3 <kevinmccoy678@gmail.com>
2025-04-09 20:24:47 -0500

    add ownership headers

--- src/cmebart.cpp
+++ src/cmebart.cpp
@@ -1,6 +1,6 @@
 /*
- *  BART: Bayesian Additive Regression Trees
- *  Copyright (C) 2017 Robert McCulloch and Rodney Sparapani
+ *  meBART: Bayesian Additive Regression Trees with Measurement Error
+ *  Copyright (C) 2025 Kevin McCoy, Zachary Wooten, and Christine Peterson
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
