5a6a9ff147d65881b3ba6444d6f01790b8474635
kmccoy3 <kevinmccoy678@gmail.com>
2025-07-29 22:36:57 -0500

    fix copyright notice

--- src/tree.cpp
+++ src/tree.cpp
@@ -1,8 +1,8 @@
 /*
  * meBART: Bayesian Additive Regression Trees with Measurement Error
  * Copyright (C) 2025 Kevin McCoy, Zachary Wooten, and Christine Peterson
- *
- * This package is a modification of the BART package originally created by 
+ * 
+ * This package is a modification of the BART package originally created by
  * Robert McCulloch and Rodney Sparapani, who own the original copyright.
  * Copyright (C) 2017 Robert McCulloch and Rodney Sparapani
  * 
@@ -10,12 +10,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, a copy is available at
  * https://www.R-project.org/Licenses/GPL-2
