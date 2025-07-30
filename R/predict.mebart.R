ad499d3c372a0ee50263be1b23cd35626fac134d
kmccoy3 <kevinmccoy678@gmail.com>
2025-07-29 23:12:43 -0500

    Clean up files / documentation

--- R/predict.mebart.R
+++ R/predict.mebart.R
@@ -20,62 +20,76 @@
 ## https://www.R-project.org/Licenses/GPL-2
 
 
-#' @title Predictions for meBART
-#' 
-#' @description This function makes predictions using a meBART model.
-#' 
+#' @title Make Predictions for meBART Model with Continuous Outcomes
+#'
+#' @description This function makes predictions using a meBART model with \emph{continuous} outcomes.
+#'
 #' @param object A meBART object.
 #' @param newdata A matrix of new data for prediction.
 #' @param mc.cores Number of cores to use for parallel processing.
 #' @param openmp Logical. If TRUE, OpenMP is used for parallel processing.
 #' @param ... Additional arguments.
-#' 
-#' @return A matrix of predictions.
-#' 
+#'
+#' @return A matrix of predictions with size (ndpost x n)
+#'
 #' @export
 #' @importFrom parallel detectCores
-#' 
+#'
 #' @examples
-#' # Example usage
-#' # Load the meBART package
-#' # library(meBART)
-#' #' # Create a meBART object
-#' #' # model <- mebart(x.train, y.train, ...)
-#' #' # Create new data for prediction
-#' #' # newdata <- matrix(...)
-#' #' # Make predictions
-#' #' #' predictions <- predict(model, newdata)
-#' #' # Print predictions
-#' #' #' print(predictions)
-#' 
-predict.mebart <- function(object, 
-    newdata, 
-    mc.cores = 1, 
-    openmp = (mc.cores.openmp() > 0), 
-    ...) {
-    ## if(class(newdata) != "matrix") stop("newdata must be a matrix")
+#' # Example usage:
+#' set.seed(0)
+#' x.train <- matrix(rnorm(1000), ncol = 10)
+#' y.train <- rnorm(100)
+#' mdl <- meBART::mebart(x.train, y.train,
+#'               ndpost = 500,
+#'               ntree = 50,
+#'               meas_error_sigma = diag(10),
+#'               x_mu = matrix(0, nrow=10, ncol=1),
+#'               x_sigma = diag(10))
+#' preds <- predict(mdl, newdata = x.train)
 
+predict.mebart <- function(object, # A meBART object.
+                           newdata, # A matrix of new data for prediction.
+                           mc.cores = 1, # Number of cores to use for parallel processing.
+                           openmp = (mc.cores.openmp() > 0), # If TRUE, OpenMP is used for parallel processing.
+                           ...) {
+    
+    # Check that new data conforms to the trained model
     p <- length(object$treedraws$cutpoints)
-
     if (p != ncol(newdata)) {
         stop(paste0("The number of columns in newdata must be equal to ", p))
     }
-
+    
+    # Detect the number of cores available
     if (.Platform$OS.type == "unix") {
-        mc.cores.detected <- detectCores()
+        mc.cores.detected <- parallel::detectCores()
     } else {
         mc.cores.detected <- NA
     }
-
-    if (!is.na(mc.cores.detected) && mc.cores > mc.cores.detected) mc.cores <- mc.cores.detected
-
+    
+    # Will max out cores at the detected number
+    if (!is.na(mc.cores.detected) && mc.cores > mc.cores.detected) {
+        mc.cores <- mc.cores.detected
+    }
+    
+    # Either use single core predictions or multicore predictions in OpenMP / C++
     if (.Platform$OS.type != "unix" || openmp || mc.cores == 1) {
         call <- pmebart
-    } else {
-        call <- mc.pmebart
+    } else { 
+        call <- mc.pmebart # Multicore predictions using parallel package in R
     }
-
-    if (length(object$mu) == 0) object$mu <- object$offset
-
-    return(call(newdata, object$treedraws, mc.cores = mc.cores, mu = object$mu, ...))
+    
+    # Not sure if this is necessary, but it is in the original code
+    if (length(object$mu) == 0) {
+        object$mu <- object$offset
+    }
+    
+    # Call the prediction function with the new data and model parameters
+    return(call(
+        newdata,
+        object$treedraws,
+        mc.cores = mc.cores,
+        mu = object$mu,
+        ...
+    ))
 }
--- R/predict.pbart.R
+++ R/predict.pbart.R
@@ -20,62 +20,90 @@
 ## https://www.R-project.org/Licenses/GPL-2
 
 
-#' @title Predictions for meBART, probit
-#' 
-#' @description This function makes predictions using a meBART model.
-#' 
+#' @title Make Predictions for meBART Model with Binary Outcomes
+#'
+#' @description This function makes predictions using a meBART model with \emph{binary} outcomes.
+#'
 #' @param object A meBART object.
 #' @param newdata A matrix of new data for prediction.
 #' @param mc.cores Number of cores to use for parallel processing.
 #' @param openmp Logical. If TRUE, OpenMP is used for parallel processing.
 #' @param ... Additional arguments.
-#' 
-#' @return A matrix of predictions.
-#' 
+#'
+#' @return A list with the following components:
+#' \item{yhat.test}{A matrix of predictions in the latent Z space with size (ndpost x n).}
+#' \item{prob.test}{A matrix of probabilities with size (ndpost x n).}
+#' \item{prob.test.mean}{A vector of mean probabilities for each test sample.}
+#' \item{binaryOffset}{The offset used in the model.}
+#'
 #' @export
 #' @importFrom parallel detectCores
-#' 
+#'
 #' @examples
-#' # Example usage
-#' # Load the meBART package
-#' # library(meBART)
-#' #' # Create a meBART object
-#' #' # model <- mebart(x.train, y.train, ...)
-#' #' # Create new data for prediction
-#' #' # newdata <- matrix(...)
-#' #' # Make predictions
-#' #' #' predictions <- predict(model, newdata)
-#' #' # Print predictions
-#' #' #' print(predictions)
-#' 
-predict.pbart <- function(object, newdata, mc.cores=1, openmp=(mc.cores.openmp()>0), ...) {
+#' # Example usage:
+#' set.seed(0)
+#' x.train <- matrix(rnorm(1000), ncol = 10)
+#' y.train <- rbinom(100, 1, 0.5)
+#' mdl <- meBART::pbart(x.train, y.train,
+#'               ndpost = 500,
+#'               ntree = 50,
+#'               meas_error_sigma = diag(10),
+#'               x_mu = matrix(0, nrow=10, ncol=1),
+#'               x_sigma = diag(10))
+#' preds <- predict(mdl, newdata = x.train)
 
-    ##if(class(newdata) != "matrix") stop("newdata must be a matrix")
+predict.pbart <- function(object, # A meBART object.
+                          newdata, # A matrix of new data for prediction.
+                          mc.cores = 1, # Number of cores to use for parallel processing.
+                          openmp = (mc.cores.openmp() > 0), # If TRUE, OpenMP is used for parallel processing.
+                          ...) {
 
+    # Check that new data conforms to the trained model
     p <- length(object$treedraws$cutpoints)
-
-    if(p!=ncol(newdata))
+    if (p != ncol(newdata)) {
         stop(paste0('The number of columns in newdata must be equal to ', p))
-
-    if(.Platform$OS.type == "unix") mc.cores.detected <- detectCores()
-    else mc.cores.detected <- NA
-
-    if(!is.na(mc.cores.detected) && mc.cores>mc.cores.detected) mc.cores <- mc.cores.detected
-
-    if(.Platform$OS.type != "unix" || openmp || mc.cores==1) call <- pmebart
-    else call <- mc.pmebart
-
-    ##return(call(newdata, object$treedraws, mc.cores=mc.cores, mu=object$binaryOffset, ...))
-
-    if(length(object$binaryOffset)==0) object$binaryOffset=object$offset
-
-    pred <- list(yhat.test=call(newdata, object$treedraws, mc.cores=mc.cores,
-                                mu=object$binaryOffset, ...))
-
+    }
+    
+    # Detect the number of cores available
+    if (.Platform$OS.type == "unix")
+        mc.cores.detected <- parallel::detectCores()
+    else
+        mc.cores.detected <- NA
+    
+    # Will max out cores at the detected number
+    if (!is.na(mc.cores.detected) && mc.cores > mc.cores.detected) {
+        mc.cores <- mc.cores.detected
+    }
+    
+    # Either use single core predictions or multicore predictions in OpenMP / C++
+    if (.Platform$OS.type != "unix" || openmp || mc.cores == 1) {
+        call <- pmebart
+    } else {
+        call <- mc.pmebart # Multicore predictions using parallel package in R
+    }
+    
+    # Not sure if this is necessary, but it is in the original code
+    if (length(object$binaryOffset) == 0) {
+        object$binaryOffset = object$offset
+    }
+    
+    # Call the prediction function with the new data and model parameters
+    pred <- list(
+        yhat.test = call(
+            newdata,
+            object$treedraws,
+            mc.cores = mc.cores,
+            mu = object$binaryOffset,
+            ...
+        )
+    )
+    
+    # Convert predictions in latent space to probabilities
     pred$prob.test <- pnorm(pred$yhat.test)
-    pred$prob.test.mean <- apply(pred$prob.test, 2, mean)
+    pred$prob.test.mean <- apply(pred$prob.test, 2, mean) # Average over ndpost draws
     pred$binaryOffset <- object$binaryOffset
-    attr(pred, 'class') <- 'pbart'
 
+    # Return results
+    attr(pred, 'class') <- 'pbart_preds'
     return(pred)
 }
