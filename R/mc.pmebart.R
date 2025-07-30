97e9338f1c5248c35d5794828c41b3dddcc3ef03
kmccoy3 <kevinmccoy678@gmail.com>
2025-07-30 00:41:24 -0500

    clean, clean, clean...

--- R/mc.pmebart.R
+++ R/mc.pmebart.R
@@ -20,94 +20,103 @@
 ## https://www.R-project.org/Licenses/GPL-2
 
 
-
-
-#' @title Multicore BART Predictions
-#' 
-#' @description This function performs multicore predictions using BART.
-#' 
+#' @title Multi-core (parallel) meBART Predictions
+#'
+#' @description This function performs multi-core (parallel) predictions for meBART models.
+#'
 #' @param x.test A matrix of test data.
-#' @param treedraws A list containing the tree draws.
-#' @param mu A numeric value to add to the predictions.
+#' @param treedraws A list containing the tree draws. (Part of the meBART model)
+#' @param mu Offset for the predictions.
 #' @param mc.cores Number of cores to use for parallel processing.
-#' @param transposed 
-#' @param dodraws Logical. If TRUE, draws are returned.
+#' @param transposed Logical. Is x.test transposed?
+#' @param dodraws Logical. If TRUE, all posterior draws are returned; if FALSE, only the mean is returned.
 #' @param nice Nice level for the process.
-#' 
-#' @return A matrix of predictions.
-#' 
-#' @export
+#'
+#' @return Prediction matrix of size (ndpost x n).
+#'
+#' @keywords internal
 #' @importFrom parallel detectCores
 #' @importFrom tools psnice
-#' 
-mc.pmebart <- function(
-    x.test, # x matrix to predict at
-    treedraws, # $treedraws from wbart
-    mu = 0, # mean to add on
-    mc.cores = 2L,
-    transposed = FALSE,
-    dodraws = TRUE,
-    nice = 19L) {
+
+mc.pmebart <- function(x.test,
+                       treedraws,
+                       mu = 0,
+                       mc.cores = 2L,
+                       transposed = FALSE,
+                       dodraws = TRUE,
+                       nice = 19L
+                       ) {
+
+    # Check if parallel processing is available
     if (.Platform$OS.type != "unix") {
         stop("parallel::mcparallel/mccollect do not exist on windows")
     }
-
-    if (!transposed) x.test <- t(bartModelMatrix(x.test))
-
+    
+    # Transpose x.test if it hasn't been already
+    if (!transposed) {
+        x.test <- t(bartModelMatrix(x.test))
+    }
+    
+    # Check that new data conforms to the trained model
     p <- length(treedraws$cutpoints)
-
     if (p != nrow(x.test)) {
         stop(paste0("The number of columns in x.test must be equal to ", p))
     }
-
-    mc.cores.detected <- detectCores()
-
-    if (!is.na(mc.cores.detected) && mc.cores > mc.cores.detected) mc.cores <- mc.cores.detected
-
+    
+    # Detect the number of cores available
+    mc.cores.detected <- parallel::detectCores()
+    
+    # Will max out cores at the detected number
+    if (!is.na(mc.cores.detected) && mc.cores > mc.cores.detected) {
+        mc.cores <- mc.cores.detected
+    }
+    
+    # Get number of test observations, limit mc.cores to that number
     K <- ncol(x.test)
     if (K < mc.cores) mc.cores <- K
+
+    # How many observations to predict per core
     k <- K %/% mc.cores - 1
+
+    # Initialize j, end index
     j <- K
+    
+    # Loop through cores
     for (i in 1:mc.cores) {
+
+        # Update starting index for this core
         if (i == mc.cores) {
-            h <- 1
+            h <- 1 # Start index for the last core
         } else {
-            h <- j - k
+            h <- j - k # Ending index minus k
         }
-        ## print(c(i=i, h=h, j=j))
-        parallel::mcparallel(
-            {
-                psnice(value = nice)
-                pmebart(matrix(x.test[, h:j], nrow = p, ncol = j - h + 1), treedraws, mu, 1, TRUE)
-            },
-            silent = (i != 1)
-        )
-        j <- h - 1
+
+        # Issue a parallel task for this core
+        parallel::mcparallel({
+            tools::psnice(value = nice);
+            pmebart(matrix(x.test[, h:j], nrow = p, ncol = j - h + 1),
+                    treedraws,
+                    mu,
+                    1,
+                    TRUE)
+        }, silent = (i != 1))
+
+        j <- h - 1 # new end index for next core
     }
-
-    ## K <- ncol(x.test)
-    ## k <- ceiling(K/mc.cores)
-    ## h <- K-k
-
-    ## parallel::mcparallel({psnice(value=nice);
-    ##     pwbart(trees, x.test[ , max(1, h):K], mu, 1, TRUE)})
-
-    ## if(mc.cores>1) for(i in 1:(mc.cores-1)) {
-    ##     parallel::mcparallel({psnice(value=nice);
-    ##         pwbart(trees, x.test[ , max(1, (h-k)):(h-1)], mu, 1, TRUE)},
-    ##         silent=TRUE)
-    ##     h <- h-k
-    ## }
-
+    
+    # Get predictions from all cores
     pred.list <- parallel::mccollect()
-    pred <- pred.list[[1]]
+    pred <- pred.list[[1]] # First core's prediction
+
+    # Check the type of prediction returned
     type <- class(pred)[1]
     if (type == "list") {
         pred <- pred[[1]]
     } else if (type != "matrix") {
         return(pred.list)
     } ## likely error messages
-
+    
+    # Combine predictions from all cores
     if (mc.cores > 1) {
         for (i in 2:mc.cores) {
             if (type == "list") {
@@ -117,13 +126,12 @@ mc.pmebart <- function(
             }
         }
     }
-    ## if(mc.cores>1) for(i in 2:mc.cores) pred <- cbind(pred, pred.list[[i]])
-
+    
+    # Return all posterior draws or just the means
     if (dodraws) {
-        return(pred)
+        return(pred) # Recall mu has already been added in pmebart
     } else {
         return(apply(pred, 2, mean))
     }
-    ## if(dodraws) return(pred+mu)
-    ## else return(apply(pred, 2, mean)+mu)
+
 }
