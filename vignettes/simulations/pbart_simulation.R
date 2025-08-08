122165da305c6c9489b2dc4645deedc97f4251dc
kmccoy3 <kevinmccoy678@gmail.com>
2025-08-08 17:44:56 -0500

    organize simulation files

---
+++ vignettes/simulations/pbart_simulation.R
@@ -0,0 +1,14 @@
+
+
+
+
+
+library(meBART)
+library(tidyverse)
+library(latex2exp)
+library(ggridges)
+library(progress)
+
+
+setwd("~/Documents/documents-main/research/rice_research/projects/BART/working_repo/meBART/vignettes")
+
